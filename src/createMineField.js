f174207cc53a85123d07f250873b96a817d743f4
ndaemy <yuyaebean@gmail.com>
2022-01-08 19:36:14 +0900

    🚧 Create 2d array and return

--- src/createMineField.js
+++ src/createMineField.js
@@ -5,12 +5,15 @@ function createMineField(fieldInfo) {
   validateField(fieldInfo);
 
   // 2차원 배열을 사용자에게 입력받은 숫자에 맞게 생성한다
+  const { width, height } = fieldInfo;
+  const field = [...Array(height)].map(() => Array(width).fill(null));
 
   // 폭탄을 2차원 배열에 랜덤하게 넣는다
 
   // 폭탄이 아닌 자리들에 알맞는 숫자를 채워넣는다
 
   // 2차원 배열을 반환한다
+  return field;
 }
 
 module.exports = createMineField;
