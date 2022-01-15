54ec8e54cbe19a393ac8f151229bc28c2b5fa932
ndaemy <yuyaebean@gmail.com>
2022-01-15 20:28:14 +0900

    ♻️ Separate code about movies

--- src/index.js
+++ src/index.js
@@ -1,77 +1,12 @@
 import express from 'express';
+import movies from "./movies";
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
-let nextId = 4;
-
-let movies = [
-  {
-    id: 1,
-    title: 'Avengers',
-  },
-  {
-    id: 2,
-    title: 'Spider-man',
-  },
-  {
-    id: 3,
-    title: 'Harry Potter',
-  },
-];
-
-app.get("/movies", (req, res) => {
-  res.json(movies);
-});
-
-app.get('/movies/:id', (req, res) => {
-  const index = movies.findIndex(movie => movie.id === +req.params.id);
-  if (index === -1) {
-    return res.json({
-      error: "That movie does not exist",
-    });
-  }
-  res.json(movies.filter(movie => movie.id === +req.params.id)[0]);
-});
-
-app.post('/movies', (req, res) => {
-  movies.push({
-    id: nextId++,
-    title: req.body.title,
-  });
-  res.json(movies);
-});
-
-app.put('/movies/:id', (req, res) => {
-  const index = movies.findIndex(movie => movie.id === +req.params.id);
-  if (index === -1) {
-    return res.json({
-      error: "That movie does not exist",
-    });
-  }
-
-  movies[index] = {
-    id: req.params.id,
-    title: req.body.title,
-  };
-  res.json(movies);
-});
-
-app.delete('/movies/:id', (req, res) => {
-  const index = movies.findIndex(movie => movie.id === +req.params.id);
-  if (index === -1) {
-    return res.json({
-      error: "That movie does not exist",
-    });
-  }
-
-  movies = movies.filter(movie => movie.id !== +req.params.id);
-  res.json({
-    result: "Movie was successfully deleted"
-  });
-});
+app.use('/movies', movies);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
